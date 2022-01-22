9cc98f467db3bbb7b26fb81b37e6376d2b45d41f
Oğuzhan Çevik <oguzhan728@gmail.com>
2022-01-22 11:03:44 +0300

    created register page

--- app.js
+++ app.js
@@ -107,6 +107,31 @@ app.use("/personel",newZimmetToPersonel);
 
 app.use("/zimmet",zimmetDokum);
 
+/////////////////////////////////////REGİSTER
+
+//show register form
+app.get("/register",function(req,res){
+	res.render("register");
+})
+//handle sign up logic
+app.post("/register",function(req,res){
+	var newUser=new User({username:req.body.username});
+	User.register(newUser,req.body.password,function(err,user){
+		if(err){
+			console.log(err);
+			
+			return res.render("register");
+		}
+		else{
+			passport.authenticate("local")(req,res,function(){
+            
+				res.redirect("/personel");
+			})
+			
+		}
+	})
+});
+
 
 
 
